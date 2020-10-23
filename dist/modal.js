c596b833e7c0e0602132fc7857dc8ef696736bea
Sajad Ahmad Nawabi <Sajad_Ahmad_Nawabi@outlook.com>
2020-10-23 13:04:46 +0430

    Update modal.js

--- dist/modal.js
+++ dist/modal.js
@@ -1,39 +1,41 @@
-function toggleModal(event){
-  let modalID = event.currentTarget.getAttribute("data-target");
-  let modal = document.querySelector(modalID);
-  let modalDialog = document.querySelector(modalID+"-dialog");
-  let modalBackdrop = document.querySelector(modalID+"-backdrop");
-  zoomIn(modalDialog);
+const toggleModal = e => {
+  const modalID       = e.currentTarget.getAttribute("data-target");
+  const modal         = document.querySelector(modalID);
+  const modalDialog   = document.querySelector(`${modalID}-dialog`);
+  const modalBackdrop = document.querySelector(`${modalID}-backdrop`);
+
   modalBackdrop.classList.toggle("hidden");
   modalBackdrop.classList.toggle("flex");
+
   if (modal.classList.contains("hidden")) {
     modal.style.display = "block";
-    setTimeout(()=>{
+
+    slideInBottom(modalDialog);
+
+    setTimeout(() => {
       modal.classList.remove("hidden");
       modal.classList.add("flex");
       modal.classList.remove("opacity-0");
       modalDialog.classList.remove("-translate-y-1");
       modalDialog.classList.add("translate-y-1");
-    },30);
+    }, 30);
   } else {
     modalDialog.classList.remove("translate-y-1");
     modalDialog.classList.add("-translate-y-1");
-    setTimeout(()=>{
+    
+    slideOutBottom(modalDialog);
+
+    setTimeout(() => {
       modal.style.display = "none";
       modal.classList.add("hidden");
       modal.classList.remove("flex");
       modal.classList.add("opacity-0");
-    },30)
+    }, 300);
   }
 }
 
-function initModal(){
-  let dataModal = document.querySelectorAll("[data-toggle='modal']");
-  for (var index = 0; index < dataModal.length; index++) {
-    dataModal[index].addEventListener("click",toggleModal);
-  }
-}
+const initModal = () => {
+  const dataModal = document.querySelectorAll("[data-toggle='modal'");
 
-(function () {
-  initModal();
-}());
+  for (let i of dataModal) i.addEventListener("click", toggleModal);
+}
