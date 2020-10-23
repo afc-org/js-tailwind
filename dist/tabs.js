e879c8f24f15961659af691db1314c4899c918d3
Sajad Ahmad Nawabi <Sajad_Ahmad_Nawabi@outlook.com>
2020-10-23 13:05:29 +0430

    Update tabs.js

--- dist/tabs.js
+++ dist/tabs.js
@@ -1,27 +1,24 @@
-function changeAtiveTab(event,activeClasses,normalClasses){
-  event.preventDefault();
-  // prepare classes for querySelectorAll and classList.remove/add
+const changeAtiveTab = (e, activeClasses, normalClasses) => {
+  e.preventDefault();
+
   normalClasses = normalClasses.split(" ").filter(item => item !== " " && item !== "");
   activeClasses = activeClasses.split(" ").filter(item => item !== " " && item !== "");
-  let queryActiveClasses = activeClasses.map(item => "." + item);
-  // get the anchor element with the data-toggle="tab" that has been clicked
-  let element = event.currentTarget;
-  let elementID = event.currentTarget.getAttribute("href");
-  // get it's counterpart tab element
-  let tabElement = document.querySelector(elementID);
-  // get the unordered list parent (grand-parent actualy)
-  let ulElement = element.parentNode.parentNode;
-  // get the current active element
-  let activeElement = ulElement.querySelector("li > a" + queryActiveClasses);
-  let activeElementID = activeElement.getAttribute("href");
-  // get the current active tab element
-  let activeTabElement = document.querySelector(activeElementID);
 
-  normalClasses.forEach((item) => {
+  let queryActiveClasses  = activeClasses.map(item => "." + item);
+  let element             = e.currentTarget;
+  let elementID           = e.currentTarget.getAttribute("href");
+  let tabElement          = document.querySelector(elementID);
+  let ulElement           = element.parentNode.parentNode;
+  let activeElement       = ulElement.querySelector("li > a" + queryActiveClasses);
+  let activeElementID     = activeElement.getAttribute("href");
+  let activeTabElement    = document.querySelector(activeElementID);
+
+  normalClasses.forEach(item => {
     activeElement.classList.add(item);
     element.classList.remove(item);
   });
-  activeClasses.forEach((item) => {
+
+  activeClasses.forEach(item => {
     activeElement.classList.remove(item);
     element.classList.add(item);
   });
@@ -32,26 +29,24 @@ function changeAtiveTab(event,activeClasses,normalClasses){
   tabElement.classList.add("block");
 }
 
-//  - activeClasses is for setting the classes for the active tab (anchor element)
-//  - normalClasses is for setting the classes for all the tabs (anchor elements) expect the
-// one that is currently active, if they are unset, then the default ones will be choosen
-// activeClasses = "bg-blue-600 text-white"
-// normalClasses = "text-blue-600 bg-transparent"
-function initTabs(activeClasses,normalClasses){
-  let dataTabs = document.querySelectorAll("[data-toggle='tab']");
-  const toggleFunction = (event) => {
-    event.preventDefault();
+const initTabs = (activeClasses, normalClasses) => {
+  const dataTabs = document.querySelectorAll("[data-toggle='tab']");
+
+  const toggleFunction = e => {
+    e.preventDefault();
+
     activeClasses = activeClasses ? activeClasses:"bg-blue-600 text-white";
     normalClasses = normalClasses ? normalClasses:"text-blue-600 bg-white";
-    changeAtiveTab(event,activeClasses,normalClasses)
+
+    changeAtiveTab(e, activeClasses, normalClasses);
   }
-  const toggleAnimation = () => Array.from(dataTabs).forEach(el => zoomIn(document.querySelector(el.attributes.href.value)));
-  for (var index = 0; index < dataTabs.length; index++) {
-    dataTabs[index].addEventListener("click",toggleFunction);
-    dataTabs[index].addEventListener("click",toggleAnimation);
+
+  const toggleAnimation = () => {
+    Array.from(dataTabs).forEach(el => fadeIn(document.querySelector(el.attributes.href.value), 300));
+  }
+
+  for (let i of dataTabs) {
+    i.addEventListener("click", toggleFunction);
+    i.addEventListener("click", toggleAnimation);
   }
 }
-
-(function () {
-  initTabs();
-}());
